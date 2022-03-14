13bf47e17fa2da9ca3ddefaba0f491b0b0cbf098
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-03-13 21:51:42 -0700

    config project
    + adding project structure

---
+++ AlboMapLocation/AlboMapLocation/Search Module/SearchLocationsVC.swift
@@ -0,0 +1,23 @@
+//
+//  SearchLocationsVC.swift
+//  AlboMapLocation
+//
+//  Created by Hiram Castro on 13/03/22.
+//
+
+import UIKit
+
+final class SearchLocationsVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configView()
+    }
+    
+    private func configView() {
+        self.view.backgroundColor = .white
+    }
+    
+}
